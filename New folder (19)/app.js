64158e60e170dda0201be4382f89e11df319d057
shahzgithub <143381478+shahzgithub@users.noreply.github.com>
2023-10-09 13:07:12 +0500

    Add files via upload

---
+++ New folder (19)/app.js
@@ -0,0 +1,97 @@
+//chapter 46-48  (Events)
+//Q1
+//answer in html page .
+
+//Q2
+//Answer in html page .
+
+//Q3
+//Answer in html page .
+
+//Q4
+//Answer in html page .
+
+//Q5
+//Answer in html page .
+
+//Q6
+//Answer in html page .
+
+//Q7
+//Answer in html page .
+
+//Q8
+//Answer in html page .
+
+//Q9
+//Answer in html page .
+
+//Q10
+//Answer in html page .
+
+//Q11
+//Answer in html page .
+
+//Q12
+//Answer in html page .
+
+//Chapter 49, 50 (Reading and Setting field values)
+//Q1
+//Answer in html page .
+
+//Q2
+//Answer in html page .
+
+//Q3
+//Answer in html page .
+
+//Q4
+//Answer in html page .
+
+//Q5
+//Answer in html page .
+
+//Q6
+//Answer in html page .
+
+//Q7
+//Answer in html page .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
